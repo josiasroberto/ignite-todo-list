20d9639101cc1f8d9977f00067d232378146ab7f
Josias Roberto <josiasroberto1@gmail.com>
2023-07-04 21:36:53 -0300

    NewTask component created

---
+++ src/components/NewTask.tsx
@@ -0,0 +1,12 @@
+import styles from './NewTask.module.css'
+
+import {PlusCircle} from '@phosphor-icons/react'
+
+export function NewTask(){
+  return(
+    <form className={styles.form}>
+      <input type="text" placeholder='Adicione uma nova tarefa'/>
+      <button type='submit'>Criar <PlusCircle size={16}/></button>
+    </form>
+  )
+}
