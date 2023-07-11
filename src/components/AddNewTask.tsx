95c26a4d21d500b78b1e0adff8e4bc5b1d01b858
Josias Roberto <josiasroberto1@gmail.com>
2023-07-11 18:43:21 -0300

    Tasks being saved in state

--- src/components/AddNewTask.tsx
+++ src/components/AddNewTask.tsx
@@ -1,12 +1,43 @@
-import styles from './AddNewTask.module.css'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 
+import styles from './AddNewTask.module.css'
 import plusLogo from '../assets/plus.svg'
 
 export function AddNewTask(){
+  const [tasks, setTasks] = useState([''])
+
+  const [newTask, setNewTask] = useState('')
+
+  function handleCreateNewTask(event: FormEvent){
+    event.preventDefault()
+
+    setTasks([...tasks, newTask])
+    setNewTask('')    
+  }
+  
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>){
+    event.target.setCustomValidity('')
+    setNewTask(event.target.value)
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>){
+    event.target.setCustomValidity('Este campo é obrigatório!')
+  }
+
+  const isNewTaskEmpty = newTask.length === 0
+
   return(
-    <form className={styles.form}>
-      <input type="text" placeholder='Adicione uma nova tarefa'/>
-      <button type='submit'>
+    <form onSubmit={handleCreateNewTask} className={styles.form}>
+
+      <input 
+        type="text" 
+        placeholder='Adicione uma nova tarefa' 
+        value={newTask}
+        onChange={handleNewTaskChange}
+        onInvalid={handleNewTaskInvalid}
+        required
+      />
+      <button type='submit' disabled={isNewTaskEmpty}>
         Criar
         <img src={plusLogo} alt="plus logo"/>
       </button>
